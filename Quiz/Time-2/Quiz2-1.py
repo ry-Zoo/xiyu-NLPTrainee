2695c67f384f2b80e8cc614514c73a4c176cd065
ry_Zoo <telezyrbowl@sina.com>
2019-07-12 13:21:45 +0800

    modeifyQuiz2-1

--- Quiz/Time-2/Quiz2-1.py
+++ Quiz/Time-2/Quiz2-1.py
@@ -1,5 +1,5 @@
 import re
-
+#test
 def Caculate(raw_str):
 	raw_str=raw_str.strip()
 	if len(raw_str)==0:
@@ -9,15 +9,22 @@ def Caculate(raw_str):
 	first_enum=[]
 	#print(str(first_sim))
 	for i in range(0,len(first_str)):
-		if "*" in first_str[i]:
-			second_str=first_str[i].split("*")
+		if "*" in first_str[i] or "/" in first_str[i]:
+			second_str=re.split(r"[*/]",first_str[i])
+			second_sim=[s for s in first_str[i] if s =="*" or s=="/"]
 			second_ls=[int(x) for x in second_str]
-			first_enum.append(second_ls[0]*second_ls[1])
+			sec_res=second_ls[0]
+			#print(second_sim)
+			for j in range(0,len(second_sim)):
+				if second_sim[j]=="*":
+					sec_res=sec_res*second_ls[j+1]
+				elif second_sim[j]=="/":
+					sec_res=sec_res/second_ls[j+1]
+				else:
+					return 0
+			#print(sec_res)
+			first_enum.append(sec_res)
 			#print(first_enum)
-		elif "/" in first_str[i]:
-			second_str=first_str[i].split("/")
-			second_ls=[int(x) for x in second_str]
-			first_enum.append(second_ls[0]/second_ls[1])
 		else:
 			first_enum.append(int(first_str[i]))
 	
@@ -30,8 +37,8 @@ def Caculate(raw_str):
 			res=first_enum[j]-first_enum[j+1]
 		else:
 			return 0
-
+	#print(res)
 	return res
 
 
-Caculate("3+2*2")
+Caculate("3+2*2*2")
